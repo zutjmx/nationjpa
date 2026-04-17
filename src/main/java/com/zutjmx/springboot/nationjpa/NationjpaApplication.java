d0f39bb930c77a2611350c8f37d626795ddc4500
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2026-04-17 13:09:24 -0600

    first commit

---
+++ src/main/java/com/zutjmx/springboot/nationjpa/NationjpaApplication.java
@@ -0,0 +1,13 @@
+package com.zutjmx.springboot.nationjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NationjpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NationjpaApplication.class, args);
+	}
+
+}
